789f666f71b7ccca46e97ac3eded8260ddcb4227
cyk318 <chenyikang2022@163.com>
2024-04-27 17:13:10 +0800

    feat: maxProfit VI

--- src/main/kotlin/org/cyk/kt/solution/leetcode/SolutionDynamic.kt
+++ src/main/kotlin/org/cyk/kt/solution/leetcode/SolutionDynamic.kt
@@ -1,5 +1,6 @@
 package org.cyk.solution.question
 
+import kotlin.concurrent.fixedRateTimer
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
@@ -346,11 +347,27 @@ class SolutionDynamic {
         return max(dp[len - 1][1], max(dp[len - 1][2], dp[len - 1][3]))
     }
 
+    //23.买卖股票的最佳时机含手续费
+    fun maxProfit6(prices: IntArray, fee: Int): Int {
+        //dp[i][0]: 第 i 天持有股票
+        //dp[i][1]: 第 i 卖出，不持有股票
+        //dp[i][2]: 第 i 天不持有股票(当天没有交易)
+        val len = prices.size
+        val dp = Array(len) { IntArray(3) }
+        dp[0][0] = -prices[0]
+        dp[0][1] = -fee
+        dp[0][2] = 0
+        for (i in 1 ..< len) {
+            dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][2] - prices[i], dp[i - 1][1] - prices[i]) )
+            dp[i][1] = dp[i - 1][0] + prices[i] - fee
+            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1])
+        }
+        return max(dp[len - 1][1], dp[len - 1][2])
+    }
 
 }
 
 fun main() {
     val arr = intArrayOf(1,2)
     val s = SolutionDynamic()
-    s.maxProfit(arr)
 }
