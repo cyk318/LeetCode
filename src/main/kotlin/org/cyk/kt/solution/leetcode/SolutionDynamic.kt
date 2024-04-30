b88d83345e5f1c81ade7e344b76a7ffe26115d4c
cyk318 <chenyikang2022@163.com>
2024-04-30 08:36:05 +0800

    feat: longestCommonSubsequence

--- src/main/kotlin/org/cyk/kt/solution/leetcode/SolutionDynamic.kt
+++ src/main/kotlin/org/cyk/kt/solution/leetcode/SolutionDynamic.kt
@@ -434,6 +434,26 @@ class SolutionDynamic {
         return result
     }
 
+
+    //最长公共子序列
+    fun longestCommonSubsequence(text1: String, text2: String): Int {
+        //dp[i][j]: text1 以第 i - 1 个字符为结尾， text2 以 j - 1 字符为结尾的最长公共子序列
+        val len1 = text1.length
+        val len2 = text2.length
+        val dp = Array(len1 + 1) { IntArray(len2 + 1) }
+        for (i in 1 .. len1) {
+            for (j in 1 .. len2) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                }
+            }
+        }
+        return dp[len1][len2]
+    }
+
+
 }
 
 fun main() {
