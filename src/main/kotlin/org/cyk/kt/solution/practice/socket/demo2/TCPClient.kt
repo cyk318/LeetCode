120e47ce04d305bd2c8e658f8a97456a1c485398
cyk318 <chenyikang2022@163.com>
2024-05-05 23:03:13 +0800

    feat: tcp-client

--- src/main/kotlin/org/cyk/kt/solution/practice/socket/demo2/TCPClient.kt
+++ src/main/kotlin/org/cyk/kt/solution/practice/socket/demo2/TCPClient.kt
@@ -1,4 +1,37 @@
 package org.cyk.kt.solution.practice.socket.demo2
 
-class TCPClient {
+import java.io.PrintWriter
+import java.net.Socket
+import java.util.Scanner
+
+class TCPClient(
+    ip: String,
+    port: Int,
+) {
+
+    private val socket = Socket(ip, port)
+
+    fun start() {
+        val systemScan = Scanner(System.`in`)
+        socket.getInputStream().use { input ->
+            socket.getOutputStream().use { output ->
+                val serverScan = Scanner(input)
+                val writer = PrintWriter(output)
+                while (true) {
+                    print("client -> ")
+                    val request = systemScan.next()
+                    writer.println(request)
+                    writer.flush()
+                    val response = serverScan.next()
+                    println("server -> $response")
+                }
+            }
+        }
+    }
+
+}
+
+fun main() {
+    val client = TCPClient("127.0.0.1", 9000)
+    client.start()
 }
