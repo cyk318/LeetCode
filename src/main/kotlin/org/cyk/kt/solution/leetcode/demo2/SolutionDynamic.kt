09adbff24b9aac9bb93e7278d26c8c4e65b5f5e2
cyk318 <chenyikang2022@163.com>
2024-05-07 09:14:39 +0800

    feat: minCostClimbingStairs

--- src/main/kotlin/org/cyk/kt/solution/leetcode/demo2/SolutionDynamic.kt
+++ src/main/kotlin/org/cyk/kt/solution/leetcode/demo2/SolutionDynamic.kt
@@ -1,5 +1,9 @@
 package org.cyk.kt.solution.leetcode.demo2
 
+import kotlin.math.cos
+import kotlin.math.max
+import kotlin.math.min
+
 class SolutionDynamic {
 
     //1.斐波那契
@@ -12,6 +16,32 @@ class SolutionDynamic {
             dp[i] = dp[i - 1] + dp[i - 2]
         }
         return dp[n]
+    }
+
+    //2.爬楼梯
+    fun climbStairs(n: Int): Int {
+        val dp = IntArray(n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for (i in 2 .. n) {
+            dp[i] = dp[i - 1] + dp[i - 2]
+        }
+        return dp[n]
+    }
+
+    //3.使用最小花费爬楼梯
+    fun minCostClimbingStairs(cost: IntArray): Int {
+        //dp定义：爬上第 n 个台阶的最小花费
+        //dp[0] = cost[0]
+        //dp[1] = cost[1]
+        val len = cost.size
+        val dp = IntArray(len + 1)
+        dp[0] = 0
+        dp[1] = 0
+        for (i in 2 .. len) {
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        }
+        return dp.last()
     }
 
 }
