d1d9fb7c4726bdfb31df2afcd01b4290e9d085a3
cyk318 <chenyikang2022@163.com>
2024-05-14 09:31:48 +0800

    feat: rob

--- leetcode/src/main/kotlin/org/cyk/kt/solution/leetcode/demo2/SolutionDynamic.kt
+++ leetcode/src/main/kotlin/org/cyk/kt/solution/leetcode/demo2/SolutionDynamic.kt
@@ -189,4 +189,14 @@ class SolutionDynamic {
         return dp[len]
     }
 
+    fun rob(nums: IntArray): Int {
+        val len = nums.size
+        val dp = IntArray(len + 1)
+        dp[1] = nums[0]
+        for (i in 2 .. len) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
+        }
+        return dp.last()
+    }
+
 }
