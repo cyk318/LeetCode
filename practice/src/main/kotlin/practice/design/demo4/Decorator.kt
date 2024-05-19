4022a1146002fa79c3e4d95d17c88ff0d3854e7e
cyk318 <chenyikang2022@163.com>
2024-05-19 23:48:03 +0800

    feat: decorator

---
+++ practice/src/main/kotlin/practice/design/demo4/Decorator.kt
@@ -0,0 +1,69 @@
+package practice.design.demo4
+
+//抽象组件
+interface Coffee {
+
+    fun getDesc(): String
+    fun getCost(): Double
+
+}
+
+//具体组件
+class SimpleCoffee: Coffee {
+
+    override fun getDesc(): String = "[ simple coffee ]"
+    override fun getCost(): Double = 6.0
+
+}
+
+//抽象装饰者
+abstract class CoffeeDecorator(
+    protected val coffee: Coffee
+): Coffee {
+
+    override fun getDesc(): String = coffee.getDesc()
+    override fun getCost(): Double = coffee.getCost()
+
+}
+
+//具体装饰者
+class MikeDecorator(
+    coffee: Coffee
+): CoffeeDecorator(coffee) {
+
+    override fun getDesc(): String = "${coffee.getDesc()}, mike"
+    override fun getCost(): Double = coffee.getCost() + 2.0
+
+}
+
+//具体装饰者
+class SugarDecorator(
+    coffee: Coffee
+): CoffeeDecorator(coffee) {
+
+    override fun getDesc(): String = "${coffee.getDesc()}, sugar"
+    override fun getCost(): Double = coffee.getCost() + 1.0
+
+}
+
+fun main() {
+    val simpleCoffee = SimpleCoffee()
+    println("simpleCoffee:")
+    println("desc: ${simpleCoffee.getDesc()}")
+    println("cost: ${simpleCoffee.getCost()}")
+
+    //加奶
+    val mikeSimpleCoffee = MikeDecorator(simpleCoffee)
+    println("mikeSimpleCoffee:")
+    println("desc: ${mikeSimpleCoffee.getDesc()}")
+    println("cost: ${mikeSimpleCoffee.getCost()}")
+
+    //加糖
+    val mikeSugarSimpleCoffee = SugarDecorator(mikeSimpleCoffee)
+    println("mikeSugarSimpleCoffee:")
+    println("desc: ${mikeSugarSimpleCoffee.getDesc()}")
+    println("cost: ${mikeSugarSimpleCoffee.getCost()}")
+
+}
+
+
