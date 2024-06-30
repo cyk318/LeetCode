0fd467716c2383d3331617b899eb57f1f2b066b3
cyk318 <chenyikang2022@163.com>
2024-06-30 14:25:37 +0800

    feat: BridgeOld

---
+++ practice/src/main/kotlin/practice/design/demo1/bridge/OldBridge.kt
@@ -0,0 +1,28 @@
+package practice.design.demo1.bridge
+
+
+//interface Phone {
+//    fun open()
+//    fun call()
+//    fun close()
+//}
+//
+//interface PhoneMini: Phone
+//interface PhonePro: Phone
+//interface PhoneProMax: Phone
+//
+//class HuaWeiPhoneMini: PhoneMini {
+//    //实现三个方法...
+//}
+//class XiaoMiPhoneMini: PhoneMini {
+//    //实现三个方法...
+//}
+//class VivoPhoneMini: PhoneMini {
+//    //实现三个方法...
+//}
+////PhonePro 和 PhoneProMax 也要实现这些手机类型 -> 类爆炸、将来不好维护
+
+
+
+
+
--- practice/src/main/kotlin/practice/design/demo1/prototype/NewDesign.kt
+++ practice/src/main/kotlin/practice/design/demo1/prototype/NewDesign.kt
@@ -1,33 +1,73 @@
 package practice.design.demo1.prototype
 
-data class Pig(
-    var name: String,
-    val age: Int,
-    val color: String,
-    val child: PigChild
-) {
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.io.Serializable
 
-    /**
-     * kotlin 的数据类默认提供了 copy() 方法，用于实现对这个对象的 浅拷贝
-     * 但如果在 copy 时传入其他参数，就会为当前对象创建新对象，但是对内的引用变量还是传入地址(Pig 的 hashCode 不一致，单 child 的hashCode 一致)
-     * 例如，复制当前对象并修改 name
-     */
-    fun copyWithId(name: String): Pig {
-        return this.copy(name = name)
+//data class Pig(
+//    val name: String,
+//    val age: Int,
+//    val color: String,
+//    val child: PigChild
+//) {
+//
+//    fun deepClone(): Pig {
+//        return Pig(
+//            name = this.name,
+//            age = this.age,
+//            color = this.color,
+//            child = child.clone(),
+//        )
+//    }
+//
+//}
+//
+//data class PigChild (
+//    var name: String,
+//) {
+//    fun clone(): PigChild {
+//        return PigChild(this.name)
+//    }
+//}
+//
+//fun main() {
+//    val p = Pig("lyj", 1, "pink", PigChild("aaa"))
+//
+//    val p2 = p.deepClone()
+//
+//    println("${p.hashCode()}, ${p.child.hashCode()}")
+//    println("${p2.hashCode()}, ${p2.child.hashCode()}")
+//}
+
+data class Child(
+    val name: String
+): Serializable
+
+data class Parent(
+    val name: String,
+    val child: Child
+): Serializable {
+
+    fun deepCopy(): Parent {
+        //序列化
+        val bo = ByteArrayOutputStream()
+        ObjectOutputStream(bo).use { it.writeObject(this) }
+        //反序列化
+        val bi = ByteArrayInputStream(bo.toByteArray())
+        return ObjectInputStream(bi).use { it.readObject() as Parent }
     }
 
 }
 
-data class PigChild (
-    var name: String,
-)
-
 fun main() {
-    val p = Pig("lyj", 1, "pink", PigChild("aaa"))
-    val p2 = p.copy("ll")
+    val p = Parent("John", Child("New York"))
 
-    println("${p.hashCode()}, ${p.child.hashCode()}")
-    println("${p2.hashCode()}, ${p2.child.hashCode()}")
+    val p1 = p.deepCopy()
+
+    println(p == p1) //true
+    println(p === p1) //false
+    println(p.child === p1.child) //false
 }
-
-
